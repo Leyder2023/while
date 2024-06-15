ba4428b67eef9d20c8790de3b544dbff8216bbbe
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:54:36 -0500

    Add files via upload

---
+++ while3/src/main/java/com/mycompany/while3/While3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.while3;
+
+/**
+ *
+ * @author john
+ */
+public class While3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
